a67cb520f98580a9c70b32c0a6ffd39029a51399
Derperinoo <derperinoo@gmail.com>
2018-09-18 19:37:38 +0800

    bs commit

--- index.js
+++ index.js
@@ -2962,7 +2962,8 @@ app.post('/webhook/', function(req, res) {
 
 				
 			}
-			if(text.includes("quezon boulevard")){
+			
+			if(text.includes(=="quezon boulevard")){
 				let chatbotResponse = "";
 				
 				//source : https://www.npmjs.com/package/axios
